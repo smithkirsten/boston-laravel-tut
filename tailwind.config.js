ae9bd53ced3622d123689551b20c1bc98ab24116
Kirsten Smith <brownsmith.kirsten@gmail.com>
2024-04-15 15:05:59 -0400

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    //all files that use tailwind need to be referenced here
+    "./resources/**/*.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
