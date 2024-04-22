30415637b349c2ab5ae6d0f7b334859723c620c3
Kirsten Smith <brownsmith.kirsten@gmail.com>
2024-04-22 09:37:00 -0400

    Add listing seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Listing;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,7 +13,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+       \App\Models\User::factory(10)->create();
+
+       Listing::create([
+            'title' => 'Test Listing',
+            'description' => 'This is a test listing',
+            'tags' => 'laravel, php, developement, backend',
+            'company' => 'Test Company',
+            'location' => 'Generic City',
+            'email' => 'test@example.com',
+            'website' => 'https://example.com',
+       ]);
+
+       Listing::create([
+        'title' => 'Example Listing',
+        'description' => 'This is a test listing',
+        'tags' => 'javascript, livewire, php, developement, frontend',
+        'company' => 'Example Company',
+        'location' => 'Generic Metro Area',
+        'email' => 'example@example.com',
+        'website' => 'https://example.com',
+       ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
